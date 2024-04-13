10194639e2052f8069868d11a51616f380b616f9
adidas820 <adaialvarez2006@gmail.com>
2024-04-13 14:25:40 +0200

    Little upgrades to the index.js

--- index.js
+++ index.js
@@ -1,70 +1,50 @@
-// Approve
-if(message.content.startsWith("/approve")){ // /approve @USER [Mistakes]
-  message.delete();
-  if(message.member.roles.cache.has("ID")){ // ID of staff role
-    let mencionado = message.mentions.members.first();
-    if (!mencionado) return message.reply("please, mention an user.")
-    message.channel.send('***✅ <@' + message.mentions.members.first() + '>***, has passed the exam of whitelist with **' + args[1] + '** mistake/s.');
+var pretag = document.getElementById("d");
 
-    let member = message.mentions.members.first();
-    member.roles.add("ID"); // Role of whitelist
+var tmr1 = undefined,
+  tmr2 = undefined;
+var A = 1,
+  B = 1;
 
-  } else {
-    message.reply("you don't have staff perms to perform this command.");
+var asciiframe = () => {
+  var b = [];
+  var z = [];
+  A += 0.07;
+  B += 0.03;
+  var cA = Math.cos(A),
+    sA = Math.sin(A),
+    cB = Math.cos(B),
+    sB = Math.sin(B);
+  for (var k = 0; k < 1760; k++) {
+    b[k] = k % 80 == 79 ? "\n" : " ";
+    z[k] = 0;
   }
-}
+  for (var j = 0; j < 6.28; j += 0.07) {
+    var ct = Math.cos(j),
+      st = Math.sin(j);
+    for (i = 0; i < 6.28; i += 0.02) {
+      var sp = Math.sin(i),
+        cp = Math.cos(i),
+        h = ct + 2,
+        D = 1 / (sp * h * sA + st * cA + 5), 
+        t = sp * h * cA - st * sA;
 
-// Exam Fail
-if(message.content.startsWith("/examfail")){ // /examfail @USER [Mistakes]
-  message.delete();
-  if(message.member.roles.cache.has("ID")){ // ID of staff role
-    let mencionado = message.mentions.members.first();
-    if (!mencionado) return message.reply("please, mention an user.")
-    message.channel.send('***❌ <@' + message.mentions.members.first() + '>***, has failed the exman of whitelist with **' + args[1] + '** mistake/s.');
-
-    let member = message.mentions.members.first();
-    member.roles.add("ID"); // Role of fail of exam
-
-  } else {
-    message.reply("you don't have staff perms to perform this command.");
+      var x = 0 | (40 + 30 * D * (cp * h * cB - t * sB)),
+        y = 0 | (12 + 15 * D * (cp * h * sB + t * cB)),
+        o = x + 80 * y,
+        N =
+          0 |
+          (8 *
+            ((st * sA - sp * ct * cA) * cB -
+              sp * ct * sA -
+              st * cA -
+              cp * ct * sB));
+      if (y < 22 && y >= 0 && x >= 0 && x < 79 && D > z[o]) {
+        z[o] = D;
+        b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
+      }
+    }
   }
-}
+  pretag.innerHTML = b.join("");
+};
 
-// Final Fail Exam
-if(message.content.startsWith("/finalfail")){ // /finalfail @USER [Mistakes]
-  message.delete();
-  if(message.member.roles.cache.has("ID")){ // ID of staff role
-    let mencionado = message.mentions.members.first();
-    if (!mencionado) return message.reply("please, mention an user.")
-    message.channel.send('***❌ <@' + message.mentions.members.first() + '>***, has failed the final exam of the whitelist with **' + args[1] + '** mistake/s.');
-      
-    mencionado.roles.add("ID"); // Role of fail of final exam
-    mencionado.roles.add("ID"); // Blacklist role
-  } else {
-    message.reply("you don't have staff perms to perform this command.");
-  }
-}
-
-// Blacklist
-if(message.content.startsWith("/blacklist")){ // /blacklist @USER [Reason]
-  message.delete();
-  if(message.member.roles.cache.has("ID")){ // ID of staff role
-    let mencionado = message.mentions.members.first();
-    if (!mencionado) return message.reply("please, mention an user.")
-    if(!args[1]) return message.reply("there's no arguments. Example: /blacklist @USER Troll and PG.");
-
-    mencionado.roles.add("ID"); // Role of blacklist
-
-    const channel = client.channels.cache.find(channel => channel.id === "ID"); // ID of channel logs
-    const blackListEmbed = {
-      description: 'The user <@' + message.mentions.members.first() + '> has been blacklisted by <@' + message.author.id + '>. Reason: ' + args.slice(1).join(' '),
-      color: 0xFF0000,
-      title: '• BlackList',
-      timestamp: new Date(),
-      footer: '• BlackList',
-    };
-    channel.send({embed: blackListEmbed});
-  } else {
-    message.reply("you don't have staff perms to perform this command.");
-  }
-}
+setInterval(asciiframe, 50)
